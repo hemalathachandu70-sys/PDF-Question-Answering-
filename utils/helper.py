c5243daab1ef5759fcf97937c84a4c869d3de602
hemalathachandu70-sys <hemalathachandu70@gmail.com>
2026-08-11 14:32:27 +0530

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -1,1 +1,16 @@
+import os
 
+from loaders.pdf_loader import load_pdf
+
+from vectorstore.vector_store import create_vectorstore
+
+from config import DB_PATH
+
+
+def initialize_database():
+
+    if not os.path.exists(DB_PATH):
+
+        docs = load_pdf("C:\\25\\data\\hema resum24 july (1).pdf")
+
+        create_vectorstore(docs)
