3da4f3b1844ffd79fbe2eb330de039954b2c07bb
hemalathachandu70-sys <hemalathachandu70@gmail.com>
2026-08-11 14:30:24 +0530

    Update prompt.py

--- prompt/prompt.py
+++ prompt/prompt.py
@@ -1,1 +1,21 @@
+from langchain_core.prompts import ChatPromptTemplate
 
+RAG_PROMPT = ChatPromptTemplate.from_template(
+"""
+You are a helpful AI Assistant.
+
+Answer ONLY using the context below.
+
+If the answer is not found, reply:
+
+"I couldn't find that information in the uploaded document."
+
+Context:
+{context}
+
+Question:
+{input}
+
+Answer:
+"""
+)
